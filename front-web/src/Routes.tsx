d1a4884a7f5d798724cd5909225557babf593b5f
Jonas Ferreira <jonas.edu@hotmail.com>
2021-01-21 18:41:17 -0300

    Add base admin form structure

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './core/components/Navbar';
-import Admin from './pages/Admin';
-import Catalog from './pages/Catalog';
-import ProductDetails from './pages/Catalog/components/ProductDetails';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Navbar from "./core/components/Navbar";
+import Admin from "./pages/Admin";
+import Catalog from "./pages/Catalog";
+import ProductDetails from "./pages/Catalog/components/ProductDetails";
+import Home from "./pages/Home";
 
 const Routes = () => (
-    <BrowserRouter>
-        <Navbar/>
-        <Switch>
-            <Route path="/" exact>
-                <Home/>
-            </Route>
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
 
-            <Route path="/products" exact>
-                <Catalog/>
-            </Route>
+      <Route path="/products" exact>
+        <Catalog />
+      </Route>
 
-            <Route path="/products/:productId">
-                <ProductDetails/>
-            </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
+      </Route>
 
-            <Route path="/admin">
-                <Admin/>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-
+      <Redirect from="/admin" to="/admin/products" exact />
+      <Route path="/admin">
+        <Admin />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
